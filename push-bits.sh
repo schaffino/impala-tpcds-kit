9a55ac0ba407cf90a15f0e0dd8f503d573ccdb2e
dradmin <dradmin@cloudera-director.mns-cluster.internal>
2016-11-22 17:36:12 -0600

    Make push bits report on failure

--- push-bits.sh
+++ push-bits.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# TODO: make sure you have set up dn.txt with your DataNode hostnames, 1 per line
-set -e
+_ERR_HDR_FMT="%.23s %s[%s]: "
+_ERR_MSG_FMT="${_ERR_HDR_FMT}%s\n"
+
+error_msg() {
+  printf "$_ERR_MSG_FMT" $(date +%F.%T.%N) ${BASH_SOURCE[1]##*/} ${BASH_LINENO[0]} "${@}"
+}
 
 cat dn.txt | while read h
 do
-  scp -rp $HOME/tpcds-kit $h:$HOME
-  scp -rp $HOME/impala-tpcds-kit $h:$HOME
+  scp -rp $HOME/tpcds-kit $h:$HOME ||  { error_msg "Failed scping  $HOME/tpcds-kit  to $h " ; exit 1; }
+  scp -rp $HOME/impala-tpcds-kit $h:$HOME { error_msg "Failed scping  $HOME/impala-tpcds-kit  to $h " ; exit 1; }
 done
